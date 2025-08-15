a3484059a20e01fed02f9ea279b1c81a3ca3df72
Junior <jumabunjhb024@student.wethinkcode.co.za>
2025-08-15 09:02:48 +0200

    feat(card-system): add number, action and wild card types

---
+++ src/main/java/tech/mabunda/card/NumberCard.java
@@ -0,0 +1,7 @@
+package tech.mabunda.card;
+
+public class NumberCard extends Card {
+    public NumberCard(String value, Color color) {
+        super(Type.NUMBER, value, color);
+    }
+}
---
+++ src/main/java/tech/mabunda/card/WildCard.java
@@ -0,0 +1,7 @@
+package tech.mabunda.card;
+
+public class WildCard extends Card {
+    public WildCard(String value) {
+        super(Type.WILD, value, null);
+    }
+}
